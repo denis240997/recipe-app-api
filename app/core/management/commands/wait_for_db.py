3a859d7c9286289561ea6fdc4fdc7cb11b5f1ced
denis240997 <hamitov.97@mail.ru>
2021-09-22 15:54:01 +0300

    wait_for_db script exposure

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,6 +5,9 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
+# This is bullshit! Problem was not solved! 
+# The first connection is successful, but after that postgres closes the connection and 
+# reconnects. At the moment, this script has already worked, so the application container crashes.
 class Command(BaseCommand):
     '''Django command to pause execution until database is avaliable'''
 
@@ -13,6 +16,7 @@ class Command(BaseCommand):
         db_connection = None
         while not db_connection:
             try:
+                time.sleep(2)    # ha-ha, yes! it's just a delay for the database to start up
                 db_connection = connections['default']
             except OperationalError:
                 self.stdout.write('Database unavailable, waiting 1 second...')
